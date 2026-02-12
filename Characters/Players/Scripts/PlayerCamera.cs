1b37763e75376c67a9f7860c83af1c510f6ede16
Stephen Melnick <stephenwmelnick@gmail.com>
2026-02-12 18:30:13 -0500

    Split camera attachment logic into separate script

--- Characters/Players/Scripts/PlayerCamera.cs
+++ Characters/Players/Scripts/PlayerCamera.cs
@@ -5,45 +5,15 @@ namespace CraterSprite;
 public partial class PlayerCamera : Camera2D
 {
     [Export] private uint _playerIndex;
-    [Export] private float _horizontalPosition;
-    [Export] private float _verticalOffset;
-    
-    // If true, only landing on floors above the current floor will trigger
-    // the current floor position to change
-    [Export] private bool _limitToFloorsAbove = true;
     
     private Node2D _target;
-
-    private float _floorHeight;
-    
     public override void _Ready()
     {
-        _target = GameMode.instance.players[(int)_playerIndex];
-        _target.TreeExiting += () => { _target = null; };
-        _horizontalPosition = _target.GlobalPosition.X;
-        _floorHeight = _target.GlobalPosition.Y;
-        GlobalPosition = _target.GlobalPosition;
+        var player = GameMode.instance.players[(int)_playerIndex];
 
-        if (_target is KinematicCharacter kinematicCharacter)
-        {
-            kinematicCharacter.onHitFloor.AddListener(TargetHitFloor);
-        }
+        var cameraAttachmentPoint = CraterFunctions.FindNodeByClass<PlayerCameraAttachmentPoint>(player);
+        cameraAttachmentPoint?.SetRemoteNode(GetPath());
+
         ResetSmoothing();
-    }
-
-    public override void _PhysicsProcess(double delta)
-    {
-        if (_target == null)
-        {
-            return;
-        }
-        
-        SetGlobalPosition(new Vector2(_horizontalPosition, Mathf.Min(_target.GlobalPosition.Y, _floorHeight)));
-    }
-
-    private void TargetHitFloor()
-    {
-        var newFloorPosition = _target.GlobalPosition.Y;
-        _floorHeight = _limitToFloorsAbove ? Mathf.Min(newFloorPosition, _floorHeight) : newFloorPosition;
     }
 }
